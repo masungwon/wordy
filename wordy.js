858cbdc1d5b5bcb6e488e0638e4e04dfe903862c
Stride Admin <strideadmin@Kuans-MBP.fios-router.home>
2019-06-05 14:28:00 -0400

    extract number parser out into a private function

--- wordy.js
+++ wordy.js
@@ -4,12 +4,17 @@ export class WordProblem {
     }
 
     answer() {
+        const [firstInt, secondInt] = this.extractNumbers();
+        return firstInt + secondInt;
+    }
+
+    extractNumbers() {
         const questionLackingQuestionMark = this.question.replace('?', '');
         const words = questionLackingQuestionMark.split(' ');
         const thirdFromLastWord = words[words.length - 3];
         const lastWord = words[words.length - 1];
         const firstInt = parseInt(thirdFromLastWord);
         const secondInt = parseInt(lastWord);
-        return firstInt + secondInt;
+        return [firstInt, secondInt];
     }
 }

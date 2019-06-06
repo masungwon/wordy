1bcbd5670d48b30531289d7ec8009168124ed302
Travis Vander Hoop <vanderhoop@me.com>
2019-06-06 08:39:49 -0600

    Pull derivation methods out of public interface, making them private

--- wordy.js
+++ wordy.js
@@ -1,22 +1,27 @@
+const determineOperator = (question) => {
+    return question.includes("plus") ? "+" : "-";
+}
+
+const extractNumbers = (question) => {
+    const questionLackingQuestionMark = question.replace('?', '');
+    const words = questionLackingQuestionMark.split(' ');
+    const thirdFromLastWord = words[words.length - 3];
+    const lastWord = words[words.length - 1];
+    const firstInt = parseInt(thirdFromLastWord);
+    const secondInt = parseInt(lastWord);
+    return [firstInt, secondInt];
+}
+
 export class WordProblem {
     constructor(question) {
         this.question = question;
     }
 
     answer() {
-        const [firstInt, secondInt] = this.extractNumbers();
+        const [firstInt, secondInt] = extractNumbers(this.question);
 
-        const operator = this.question.includes("plus") ? "+" : "-"
+        const operator = determineOperator(this.question);
         return eval(`${firstInt} ${operator} ${secondInt}`);
     }
 
-    extractNumbers() {
-        const questionLackingQuestionMark = this.question.replace('?', '');
-        const words = questionLackingQuestionMark.split(' ');
-        const thirdFromLastWord = words[words.length - 3];
-        const lastWord = words[words.length - 1];
-        const firstInt = parseInt(thirdFromLastWord);
-        const secondInt = parseInt(lastWord);
-        return [firstInt, secondInt];
-    }
 }
